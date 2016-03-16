f696ebf6d2ace38da169c9ad4d52ca4a874b3efd
Jan Hřídel <hridel@gmail.com>
2016-03-16 10:00:01 +0100

    init commit

---
+++ app/app.component.ts
@@ -0,0 +1,37 @@
+import { Component }       from 'angular2/core';
+import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from 'angular2/router';
+import { HeroService }     from './hero.service';
+import { HeroesComponent } from './heroes.component';
+import {DashboardComponent} from "./dashboard.component";
+import {HeroDetailComponent} from "./hero-detail.component";
+
+@RouteConfig([
+    {path: '/dashboard', name: 'Dashboard', component: DashboardComponent, useAsDefault: true},
+    {path: '/detail/:id', name: 'HeroDetail', component: HeroDetailComponent},
+    {path: '/heroes', name: 'Heroes', component: HeroesComponent}
+])
+
+
+@Component({
+    selector: 'my-app',
+    template: `
+      <h1>{{title}}</h1>
+      <nav>
+        <a [routerLink]="['Dashboard']">Dashboard</a>
+        <a [routerLink]="['Heroes']">Heroes</a>
+      </nav>
+      <router-outlet></router-outlet>
+    `,
+    styleUrls: ['app/app.component.css'],
+
+    directives: [ROUTER_DIRECTIVES],
+    providers: [
+        ROUTER_PROVIDERS,
+        HeroService
+    ]
+
+})
+
+export class AppComponent {
+    title = 'Tour of Heroes';
+}
---
+++ app/heroes.component.ts
@@ -0,0 +1,38 @@
+import {Component, OnInit} from 'angular2/core';
+import {Hero} from './hero';
+import {HeroDetailComponent} from './hero-detail.component';
+import {HeroService} from './hero.service';
+import {Router} from "angular2/router";
+@Component({
+    selector: 'my-heroes',
+    templateUrl: 'app/heroes.component.html',
+    styleUrls: ['app/heroes.component.css'],
+    directives: [HeroDetailComponent]
+})
+export class HeroesComponent implements OnInit {
+    heroes:Hero[];
+    selectedHero:Hero;
+
+    constructor(
+        private _heroService:HeroService,
+        private _router:Router
+    ) {
+    }
+
+    getHeroes() {
+        this._heroService.getHeroes().then(heroes => this.heroes = heroes);
+        //this._heroService.getHeroesSlowly().then(heroes => this.heroes = heroes);
+    }
+
+    ngOnInit() {
+        this.getHeroes();
+    }
+
+    onSelect(hero:Hero) {
+        this.selectedHero = hero;
+    }
+
+    gotoDetail() {
+        this._router.navigate(['HeroDetail', { id: this.selectedHero.id }]);
+    }
+}
